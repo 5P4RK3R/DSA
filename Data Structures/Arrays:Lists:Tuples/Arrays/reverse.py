e8f2909843d3cd35713c36ae9a5942369302320c
5P4RK3R <mahendransparker@gmail.com>
2023-07-22 19:12:05 +0530

    user input added

--- Data Structures/Arrays:Lists:Tuples/Arrays/reverse.py
+++ Data Structures/Arrays:Lists:Tuples/Arrays/reverse.py
@@ -1,4 +1,9 @@
-a = [1,4,3,2]
 def reverse(data):
     return data[::-1]
-print(reverse(a))
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    print(reverse(arr))
